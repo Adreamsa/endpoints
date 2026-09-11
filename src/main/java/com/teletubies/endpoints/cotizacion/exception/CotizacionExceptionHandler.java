961bb6575e10c5f33eee88835abc6c47773df1b8
edgardr123 <eduranrivadeneyra@gmail.com>
2026-09-11 17:20:43 -0600

    Manejo de errores por campos inválidos

--- src/main/java/com/teletubies/endpoints/cotizacion/exception/CotizacionExceptionHandler.java
+++ src/main/java/com/teletubies/endpoints/cotizacion/exception/CotizacionExceptionHandler.java
@@ -1,14 +1,17 @@
 package com.teletubies.endpoints.cotizacion.exception;
 
 import com.teletubies.endpoints.api.CotizacionApiController;
+import com.teletubies.endpoints.model.ErrorDetalleResource;
 import com.teletubies.endpoints.model.ErrorResource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.OffsetDateTime;
+import java.util.List;
 
 @Slf4j
 @RestControllerAdvice(assignableTypes = CotizacionApiController.class)
@@ -26,5 +29,29 @@ public class CotizacionExceptionHandler {
                 .build();
 
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(cuerpo);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResource> manejarValidacion(final MethodArgumentNotValidException ex) {
+        final List<ErrorDetalleResource> detalles = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fe -> ErrorDetalleResource.builder()
+                        .campo(fe.getField())
+                        .mensaje(fe.getDefaultMessage())
+                        .build())
+                .toList();
+
+        log.warn("Peticion rechazada por validacion: {} error(es)", detalles.size());
+
+        final ErrorResource cuerpo = ErrorResource.builder()
+                .timestamp(OffsetDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .codigo("VALIDACION_FALLIDA")
+                .mensaje("La peticion tiene campos invalidos")
+                .detalles(detalles)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(cuerpo);
     }
 }
